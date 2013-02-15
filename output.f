2cab987f8c6f85199002af377c5984bdcb7ec6fb
João Faria <Joao.Faria@astro.up.pt>
2013-02-15 18:46:49 +0000

    fixed output to more parameters

--- output.f
+++ output.f
@@ -16,7 +16,7 @@
     character(len=80) :: afile
     character(len=1)  :: amess
 
-    real(dp)            :: taud
+    real(dp)            :: tau_bcz, tau_he
     real(dp)            :: chi2, chi2norm
 
     real(dp), dimension(150) :: xx, resultfun
@@ -25,29 +25,33 @@
     integer :: nfile, length, i
 
 
-    taud = c(1) / (w0ref*fac)
+    tau_bcz = c(1) / (w0ref*fac)
+    tau_he = c(4) / (w0ref*fac)
+    
     call get_chi_square(chi2, chi2norm)
 
     write (6,*) "  Frequencies from file: ", afile
     
-    write(*,*) taud, c(1)
-    
     write (6,1010) 'Results:', &
-                        'Tau_d = ', taud, 'Phi_d = ', c(2), &
-                        'A_d = ', c(3), &
+                        'tau_BCZ = ', tau_bcz, 'Phi = ', c(2), &
+                        'A_BCZ = ', c(3), &
+                        'tau_HeII = ', tau_he, 'Phi = ', c(5), &
+                        'A_HeII = ', c(6), 'beta_HeII = ', c(7), &
                         'chi2 = ', chi2, 'chi2norm = ', chi2norm
 
  1010   format (3x, a, //, &
-                6x, a, f9.4, 6x, a, f8.5, //, &
-                6x, a, f10.4, //, &
+                6x, a11, f10.2, 6x, a, f8.5, //, &
+                6x, a11, f10.4, //, &
+                6x, a11, f10.2, 6x, a, f8.5, //, &
+                6x, a11, f10.4, 6x, a, f8.2, //, &
+                //, &
                 6x, a, f15.3, 5x, a, f15.3, /)
+
 
     ! ploting the results
     !   create array with smooth function
     min_xx = minval(w(1:n))! - 1.0d-4
     max_xx = maxval(w(1:n))! + 1.0d-4
-!    write(*,*) min_xx*w0ref, max_xx*w0ref
-!    write(*,*) n
     call linspace(min_xx, max_xx, xx)
     do i=1,150
         resultfun(i) = fun(xx(i))
@@ -65,7 +69,7 @@
 
     ! output to "res" file -
     if (intype.eq.0) then
-        write (9,9003) afile(nfile-6:nfile-4), taud, c(2), c(3), amess
+        write (9,9003) afile(nfile-6:nfile-4), tau_bcz, c(2), c(3), amess
  9003   format (3x, a, x, f9.4, 2x, f7.5, 2x, f10.8, 2x, a1)
     endif
 
