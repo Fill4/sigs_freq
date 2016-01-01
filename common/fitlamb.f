1434f1a7f71a133d3abd0dd3e489e39f60961003
Filipe <filipe4r@gmail.com>
2016-01-01 21:16:26 +0000

    Removed seed definition from lambda cycle. Seed creation is based on TIME funtion. Needs to be changed for a random generator

--- common/fitlamb.f
+++ common/fitlamb.f
@@ -7,91 +7,92 @@
 !    we call the genetic algorithm to find the parameters that minimize the 
 !    residuals. 
 
-    use types_and_interfaces
-    use commonvar, only : nconst, iprint, lambda_iter_max, lambda, w0ref,fac, iterfit
-    use commonarray, only : c
-    
-    use lib_io
-    use lib_pikaia12
+	use types_and_interfaces
+	use commonvar, only : nconst, iprint, lambda_iter_max, lambda, w0ref,fac, iterfit
+	use commonarray, only : c
+	
+	use lib_io
+	use lib_pikaia12
 
-    implicit none
-    
-    real(dp), intent(in)            :: initial_lambda
-    real(dp), intent(inout)         :: chi2
+	implicit none
+	
+	real(dp), intent(in)            :: initial_lambda
+	real(dp), intent(inout)         :: chi2
 
-    character(len=80)               :: outfile
-
-
-    real    :: ctrl(12), x(nconst), f
-    integer :: lambda_iter, seed, exit_status, new_unit
+	character(len=80)               :: outfile
 
 
-    !! output file
-    new_unit = next_unit()
-    open (new_unit, file='lamda-chi2.dat', status='unknown', POSITION='APPEND')
+	real    :: ctrl(12), x(nconst), f
+	integer :: lambda_iter, seed, exit_status, new_unit
+
+
+	!! output file
+	new_unit = next_unit()
+	open (new_unit, file='lamda-chi2.dat', status='unknown', POSITION='APPEND')
+	seed=TIME() !Filipe - Seed fora do ciclo de lambda para garantir os mesmos numeros aleatorios para diferentes lambdas mas diferentes seeds para diferentes execucoes
 
 !    if (iprint.ge.1) call writeout (2,c)
 
-    do lambda_iter = 1, lambda_iter_max
+	do lambda_iter = 1, lambda_iter_max
 
-        ! reduce the smoothing parameter by a factor of 2 in each iteration
-        lambda = initial_lambda / 1.2d0**(lambda_iter - 1)
-        write(*,*) lambda
-        
-        ! initialize the random-number generator
-        seed=135900
-        call rninit(seed)
-        
-        ! set control variables
-        ctrl(1:12) = -1
-        ctrl(1) = 50
-        ctrl(2) = iterfit
-        ctrl(5) = 5 ! one-point+creep, adjustable rate based on fitness
-        !ctrl(12) = 2
-        outfile = 'param_file'
+		! reduce the smoothing parameter by a factor of 2 in each iteration
+		lambda = initial_lambda / 1.2d0**(lambda_iter - 1)
+		write(*,*) lambda
+		
+		! initialize the random-number generator
+		
+		call rninit(seed)
+		
+		! set control variables
+		ctrl(1:12) = -1
+		ctrl(1) = 50
+		ctrl(2) = iterfit
+		ctrl(5) = 5 ! one-point+creep, adjustable rate based on fitness
+		!ctrl(12) = 2
+		outfile = 'param_file'
 
-        ! now call pikaia
-        CALL pikaia(objfun_ga, nconst, ctrl, x, f, exit_status)
+		! now call pikaia
+		CALL pikaia(objfun_ga, nconst, ctrl, x, f, exit_status)
 
-        ! rescaling parameters
-        call rescale(x, c)
-        
-        
+		! rescaling parameters
+		call rescale(x, c)
+		
+		
 !        !     Print the results
 !        WRITE(*,*) ' status: ', exit_status
 !        WRITE(*,*) '      x: ', c
 !        WRITE(*,*) '      x: ', c(1)/(w0ref*fac), c(4)/(w0ref*fac), c(7)/(w0ref*fac)
 !        WRITE(*,*) '  chi^2: ', 1./f
 !        WRITE(*,*) ctrl
-            
+			
 
-        ! print lambda, chi^2 and parameters to file *********
-        write(new_unit, '(es12.2, f10.2, 4f10.4)') lambda, 1.0/f, &
-                                           c(1)/(w0ref*fac), c(4)/(w0ref*fac), &
-                                           c(3), c(6) * (sin(c(7)))**2
-        call flush(new_unit)
-        !**************************************
+		! print lambda, chi^2 and parameters to file *********
+		write(new_unit, '(es12.2, f10.2, 4f10.4)') lambda, 1.0/f, &
+										   c(1)/(w0ref*fac), c(4)/(w0ref*fac), &
+										   c(3), c(6) * (sin(c(7)))**2
+		call flush(new_unit)
+		!**************************************
 
-        if (exit_status /= 0) then
-            write(6,*) "Error in PIKAIA"
-            stop
-        endif
-
-
-        if (iprint.ge.1) write (3,*) ' '
-        
-    end do
+		if (exit_status /= 0) then
+			write(6,*) "Error in PIKAIA"
+			stop
+		endif
 
 
-    if (iprint.ge.3) call writeout (1)
+		if (iprint.ge.1) write (3,*) ' '
+		
+	end do
 
-    if (iprint.ge.1) write (6,*) ' '
 
-    chi2 = 1.0_dp / f
+	if (iprint.ge.3) call writeout (1)
 
-    close(new_unit)
-    
-    return
+	if (iprint.ge.1) write (6,*) ' '
+
+	chi2 = 1.0_dp / f
+
+	close(new_unit)
+	
+	return
 
   end subroutine fitlamb
   
@@ -101,77 +102,79 @@
 ! This function calculates the objective function value, i.e. the chi^2.
 ! The signal with the current parameters P is subtracted from the frequencies
 ! and the result is smoothed by a polynomial before calculating the chi^2
-    
-    use types_and_interfaces, only: dp, fun, rescale
-    use commonvar, only : iprint, lambda, use_error_chi2
-    use commonarray
-    
-    implicit none
-    
-    integer, intent(in) :: npar ! size of parameter space
-    real, intent(in)    :: p(:)
-    real                :: fun_val
-    
-    real(dp)    :: ww, sf, resid
-    integer     :: i
-    
-    ! rescaling parameters
-    call rescale(p, c)
-    
-    ! subtract the signal (given by FUN, with current values of C)
-    ! and then smooth again with current XLAMB -
-    call subtract_and_smooth(lambda)
+	
+	use types_and_interfaces, only: dp, fun, rescale
+	use commonvar, only : iprint, lambda, use_error_chi2
+	use commonarray
+	
+	implicit none
+	
+	integer, intent(in) :: npar ! size of parameter space
+	real, intent(in)    :: p(:)
+	real                :: fun_val
+	
+	real(dp)    :: ww, sf, resid
+	integer     :: i
+	
+	! rescaling parameters
+	call rescale(p, c)
+	
+	! subtract the signal (given by FUN, with current values of C)
+	! and then smooth again with current XLAMB -
+	call subtract_and_smooth(lambda)
 
-    !** PRINT **
-    if (iprint.eq.5) then
-        call writeout (3)
-        write(*,*) c
-        write(*,*) 'stoping'
-        stop
-    endif
-    !***********
+	!** PRINT **
+	if (iprint.eq.5) then
+		call writeout (3)
+		write(*,*) c
+		write(*,*) 'stoping'
+		stop
+	endif
+	!***********
 
-    resid = 0.0d0
-    ! if using errors -
-    if (use_error_chi2) then
-        do i=1,n
-            ww = w(i)
-            sf = fun(ww)
-            resid = resid + ((sd(i)-sf)/sig(i))**2
-        end do
-    ! if not using errors -
-    else if (.not. use_error_chi2) then
-        do i=1,n
-            ww = w(i)
-            sf = fun(ww)
-            resid = resid + (sd(i)-sf)**2
-        end do
-    end if
+	resid = 0.0d0
+	! if using errors -
+	if (use_error_chi2) then
+		do i=1,n
+			ww = w(i)
+			sf = fun(ww)
+			resid = resid + ((sd(i)-sf)/sig(i))**2
+		end do
+	! if not using errors -
+	else if (.not. use_error_chi2) then
+		do i=1,n
+			ww = w(i)
+			sf = fun(ww)
+			resid = resid + (sd(i)-sf)**2
+		end do
+	end if
+	print *, n
+	print *, resid
 
-    fun_val = sngl(1.0 / resid)
+	fun_val = sngl(1.0 / resid)
 
   end function objfun_ga
 
 
 
   subroutine rescale(array_in, array_out)
-        
-        use types_and_interfaces, only: dp
-        use commonvar, only: pi, w0ref, fac
-        
-        implicit none
-        
-        real, dimension(:), intent(in)      :: array_in
-        real(dp), dimension(:), intent(out) :: array_out
+		
+		use types_and_interfaces, only: dp
+		use commonvar, only: pi, w0ref, fac
+		
+		implicit none
+		
+		real, dimension(:), intent(in)      :: array_in
+		real(dp), dimension(:), intent(out) :: array_out
 
-        array_out(1) = dble(array_in(1)) * (4000.*w0ref*fac - 2000.*w0ref*fac) + 2000.*w0ref*fac
-        array_out(2) = dble(array_in(2)) * pi
-        array_out(3) = dble(array_in(3))
-        
-        array_out(4) = dble(array_in(4)) * (2000.*w0ref*fac - 500.*w0ref*fac) + 500.*w0ref*fac
-        array_out(5) = dble(array_in(5)) * pi
-        array_out(6) = dble(array_in(6)) * 5.0_dp
-        array_out(7) = dble(array_in(7)) * (300.*w0ref*fac - 100.*w0ref*fac) + 100.*w0ref*fac
+		array_out(1) = dble(array_in(1)) * (4000.*w0ref*fac - 2000.*w0ref*fac) + 2000.*w0ref*fac
+		array_out(2) = dble(array_in(2)) * pi
+		array_out(3) = dble(array_in(3))
+		
+		array_out(4) = dble(array_in(4)) * (2000.*w0ref*fac - 500.*w0ref*fac) + 500.*w0ref*fac
+		array_out(5) = dble(array_in(5)) * pi
+		array_out(6) = dble(array_in(6)) * 5.0_dp
+		array_out(7) = dble(array_in(7)) * (300.*w0ref*fac - 100.*w0ref*fac) + 100.*w0ref*fac
   
   
   end subroutine rescale
